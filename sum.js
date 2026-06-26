fec19a3b9876da4fde5d90e68ce06d56e981d222
prajvalkhadse <prajvalkhadse2017@gmail.com>
2026-06-26 16:20:31 +0530

    Second Commit

---
+++ sum.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = 8;
+
+let sum = a + b;
+
+console.log(sum)
+
+// Using Function
+
+function sumNum(a , b) {
+    let sum = a + b;
+    return sum
+}
+
+console.log(sumNum(2,4));
