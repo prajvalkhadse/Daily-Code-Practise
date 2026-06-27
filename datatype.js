d1c01e8a47b914343e02a7ad32a1e74b26a87d00
prajvalkhadse <prajvalkhadse2017@gmail.com>
2026-06-27 22:48:16 +0530

    dataype of js done

---
+++ datatype.js
@@ -0,0 +1,49 @@
+// There are 8  datatype in js
+// There are two subpart Primitive and Non- Primitive
+
+//1. Number
+let num = 1233
+
+// 2. String
+let name = " Prajval"
+
+// 3. Boolean
+
+let isAlive = true
+
+// Bigint
+
+let number = 342385624785624582525252n
+
+// Null
+
+let bob = null
+
+// Undefined 
+
+let sum = undefined
+
+// Symbol
+
+// Object it Called a Non-Primitive data type
+// Array 
+
+let arr = [1,2,3,4,5]
+console.log(arr)
+
+// Object
+
+const student = {
+    name:"Prajval",
+    city:"Germany",
+    Age:"Unknwon"
+}
+
+console.log(student.Age)
+
+// Function
+
+function sumOfNum(a,b) {
+    return a + b;
+}
+console.log(sumOfNum(5,7))
