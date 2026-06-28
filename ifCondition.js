8a5d52a35c96a437f9539c0a2ebcfa38bdda5d51
prajvalkhadse <prajvalkhadse2017@gmail.com>
2026-06-28 21:34:25 +0530

    Condition Program added

---
+++ ifCondition.js
@@ -0,0 +1,12 @@
+// Use the JavaScript if statement to execute a block of code when a condition is true.
+
+// If Else
+
+let age = 18;
+
+if(age > 18) {
+    console.log("Not Eligible to Vote")
+}
+else {
+    console.log("Eligible to vote")
+}
