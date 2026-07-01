2788b5505309857b6333ab29ade125c58196c38f
prajvalkhadse <prajvalkhadse2017@gmail.com>
2026-07-01 21:45:37 +0530

    Object file added

---
+++ object.js
@@ -0,0 +1,20 @@
+// Objeect - Objects are variables that can store both values and functions.
+
+// Values are stored as key:value pairs called properties.
+
+// Functions are stored as key:function() pairs called methods.
+
+const car = {
+    name:"Fiat",
+    Model:4033,
+    Color:"Maroon"
+}
+console.log(car)
+//You can also create an empty object, and add the properties later
+//Create an Object
+const person = {};
+
+// Add Properties
+person.firstName = "John";
+person.lastName = "Doe";
+console.log(person)
